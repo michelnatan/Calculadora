d3c2b80d77631bc971db8c34b67e829e486d2ca0
Michel Natan Feyh <63029905+michelnatan@users.noreply.github.com>
2021-06-27 14:58:18 -0300

    Update calculadora_v1.py

--- calculadora_v1.py
+++ calculadora_v1.py
@@ -3,10 +3,6 @@ import os
 
 # Calculadora em Python
 
-# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos 2 e 3. 
-# A solução será apresentada no próximo capítulo!
-# Assista o vídeo com a execução do programa!
-
 print("\n******************* Python Calculator *******************\n")
 
 continuar = 's'
